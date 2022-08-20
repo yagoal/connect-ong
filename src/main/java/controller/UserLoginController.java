d2cbace21bdd4b185e9b778f132126ff8d6f5423
ValLins18 <valdemilson.lins@gmail.com>
2022-08-20 10:59:14 -0300

    alterações de estilos

--- src/main/java/controller/UserLoginController.java
+++ src/main/java/controller/UserLoginController.java
@@ -2,8 +2,6 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.ResultSet;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
