9deab953275a501719cd7b9fcfa3d3e1f4aa0d5a
yagoal <br.yago@gmail.com>
2022-10-30 21:33:10 -0300

    feat: controller JSON

---
+++ src/main/java/model/ListAnimals.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.List;
+
+public class ListAnimals {
+
+	private List<Animal> animals;
+
+	public ListAnimals(List<Animal> animals) {
+		super();
+		this.animals = animals;
+	}
+
+	public List<Animal> getAnimals() {
+		return animals;
+	}
+
+	public void setAnimals(List<Animal> animals) {
+		this.animals = animals;
+	}
+
+}
